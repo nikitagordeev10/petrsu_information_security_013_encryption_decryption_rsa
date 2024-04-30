c03fb92bf55b3efe4ccb813dfd4e1f64db33e8f0
nikitagordeev10 <nikitagordeev10@yandex.ru>
2024-05-01 00:15:32 +0300

    lab-2 первая рабочая версия готова

--- lab_2/3_develop/test.py
+++ lab_2/3_develop/test.py
@@ -2,50 +2,37 @@ import unittest
 from diffie_hellman import Random, Math, DiffieHellman
 
 class TestRandom(unittest.TestCase):
+
     def setUp(self):
         self.random = Random(seed=42)
 
-    def test_getrandbits(self):
-        self.assertEqual(self.random.getrandbits(5), 10)
-        self.assertEqual(self.random.getrandbits(8), 170)
-        self.assertEqual(self.random.getrandbits(10), 682)
-        self.assertRaises(ValueError, self.random.getrandbits, 4)
-
-    def test_randrange(self):
-        self.assertEqual(self.random.randrange(5, 11), 7)
-        self.assertEqual(self.random.randrange(5, 21, 5), 5)
-        self.assertRaises(ValueError, self.random.randrange, 5)
-
-    def test_randint(self):
-        self.assertEqual(self.random.randint(5, 10), 7)
-        self.assertEqual(self.random.randint(10, 10), 10)
-
-    def test_generate_large_number(self):
-        self.assertTrue(2 ** 511 <= self.random.generate_large_number(512) < 2 ** 512)
-        self.assertTrue(2 ** 511 <= self.random.generate_large_number(1024) < 2 ** 1024)
-
     def test_generate_prime(self):
+        # Убеждаемся, что числа, сгенерированные методом generate_prime, действительно простые.
         self.assertTrue(Math().is_prime(self.random.generate_prime(512)))
         self.assertTrue(Math().is_prime(self.random.generate_prime(1024)))
 
-
 class TestMath(unittest.TestCase):
+    """Тесты для класса Math."""
+
     def setUp(self):
         self.math = Math()
 
     def test_pow(self):
+        # Проверяем возведение в степень по модулю.
         self.assertEqual(self.math.pow(2, 3, 5), 3)
         self.assertEqual(self.math.pow(2, 10, 11), 1)
         self.assertEqual(self.math.pow(3, 4), 81)
         self.assertEqual(self.math.pow(5, 3, 7), 6)
 
     def test_gcd(self):
+        # Проверяем нахождение наибольшего общего делителя.
         self.assertEqual(self.math.gcd(10, 25), 5)
         self.assertEqual(self.math.gcd(14, 28), 14)
         self.assertEqual(self.math.gcd(15, 17), 1)
         self.assertEqual(self.math.gcd(25, 100), 25)
 
     def test_is_prime(self):
+        # Проверяем, что числа правильно определяются как простые или составные.
         self.assertTrue(self.math.is_prime(7))
         self.assertTrue(self.math.is_prime(13))
         self.assertTrue(self.math.is_prime(23))
@@ -58,18 +45,20 @@ class TestDiffieHellman(unittest.TestCase):
         self.diffie_hellman = DiffieHellman()
 
     def test_create_module_and_generator(self):
+        # Проверяем корректность генерации простого модуля и генератора.
         p, g = self.diffie_hellman.create_module_and_generator()
         self.assertTrue(Math().is_prime(p))
         self.assertTrue(2 < g < p)
 
     def test_generate_keys(self):
+        # Проверяем генерацию закрытого и открытого ключей.
         p, g = self.diffie_hellman.create_module_and_generator()
         private_key, public_key = self.diffie_hellman.generate_keys(g, p)
         self.assertTrue(1 < private_key < p - 1)
 
-
     def test_generate_shared_secret(self):
-        p, g = 23, 5  # example values for easier testing
+        # Тест метода generate_shared_secret
+        p, g = 23, 5
         private_key_A, public_key_A = self.diffie_hellman.generate_keys(g, p)
         private_key_B, public_key_B = self.diffie_hellman.generate_keys(g, p)
 
